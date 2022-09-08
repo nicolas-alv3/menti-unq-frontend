c32ea384ebc1ce42884771c9fe271619a8a57b9e
luca <lhazuca@gmail.com>
2022-09-08 00:36:05 -0300

    Agregar login y logout con Auth0

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import './App.css';
 import {AppBar, Box, Button, Container, css, Toolbar, Typography} from "@mui/material";
+import {useAuth0} from "@auth0/auth0-react";
+
+function LoginButton() {
+    const {loginWithRedirect} = useAuth0();
+
+    return <Button onClick={loginWithRedirect} sx={{color: "white"}}>
+        Ingresar
+    </Button>;
+}
+
+function LogoutButton() {
+    const {logout} = useAuth0();
+
+    return <Button onClick={logout} sx={{color: "white"}}>
+        Logout
+    </Button>;
+}
 
 function Header() {
+    const {isAuthenticated} = useAuth0()
     return <AppBar component='nav' position={"static"}>
         <Toolbar>
             <Typography variant={'h4'}>MENTI - UNQ</Typography>
-            <Button sx={{color:'white'}}>
-                Ingresar
-            </Button>
+            {isAuthenticated ? <LogoutButton/> : <LoginButton/>}
         </Toolbar>
     </AppBar>;
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Auth0Provider} from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Auth0Provider domain='lhazuca.auth0.com' clientId='0jYDJNY7gVPvMq4XHYEgbbA3sqePLj3C'
+                       redirectUri={window.location.origin}>
+            <App/>
+        </Auth0Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
