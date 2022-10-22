3566e0f18876a305f90f1045a60109c79eb0ef6c
Nico Alvarez <nicolas.alv3@gmail.com>
2022-10-22 14:08:04 -0300

    Agrega validaciones

--- src/components/FindByCodePage.jsx
+++ src/components/FindByCodePage.jsx
@@ -4,16 +4,22 @@ import React from 'react';
 
 export default function FindByCodePage() {
     const [id, setId] = React.useState("");
+    const [submitted, setSubmitted] = React.useState(false);
 
     const navigate = useNavigate();
 
-    const handleSubmit = () => navigate(`/presentacion/${id}`)
+    const handleSubmit = () => {
+        setSubmitted(true);
+        if(id) {
+            navigate(`/presentacion/${id}`)
+        }
+    }
 
     return <Container
         sx={{display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", gap: "2em"}} maxWidth={"md"}>
         <Typography sx={{fontWeight: 500}} variant={"h2"}>Unquimeter</Typography>
         <Typography variant={"h5"}>Ingresa el código</Typography>
-        <TextField label="Código" fullWidth placeholder={"1234 5678"} onChange={e => setId(e.target.value)} value={id}
+        <TextField label="Código" fullWidth helperText={"Este campo es requerido"} error={!id && submitted} placeholder={"1234 5678"} onChange={e => setId(e.target.value)} value={id}
                    variant="outlined" />
         <Button size={"large"} fullWidth variant={"contained"} onClick={handleSubmit}>Ingresar</Button>
         <Typography variant={"body2"}>El código se encuentra en la pantalla en frente suyo</Typography>
