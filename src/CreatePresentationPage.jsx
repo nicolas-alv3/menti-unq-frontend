0b3d8d291310605314e5deefb954a1faa47bf947
luca <lhazuca@gmail.com>
2022-10-25 10:32:51 -0300

    Mejora como se ve la tab seleccionada

--- src/CreatePresentationPage.jsx
+++ src/CreatePresentationPage.jsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import {useState} from "react";
 import {Header} from "./components/Header";
-import {Box, Button, Container, Divider, Input, InputBase, Paper, Tab, Tabs} from "@mui/material";
+import {Box, Button, Container, Divider, InputBase, Paper, Tab, Tabs, Typography} from "@mui/material";
 import PropTypes from "prop-types";
+import MultipleChoiceIcon from "@mui/icons-material/Leaderboard"
 import PresentationService from "./service/PresentationService";
 import {useNavigate} from "react-router";
 import {MCQPanel} from "./components/MCQPanel";
@@ -12,19 +13,22 @@ function SlidesPanel({slides, slideChange}) {
     const [selectedTab, setSelectedTab] = useState(0);
     return <>
         <Tabs value={selectedTab} orientation='vertical' variant='scrollable'
-              sx={{borderRight: 1, borderColor: 'black', width: '10em', padding: '1em 0', height: '90%'}}
+              sx={{borderColor: 'black', width: '10em', height: '90%'}}
         >
             {slides.map((_slide, i) =>
                 <Tab key={i.toString()} component={() => <Container onClick={() => setSelectedTab(i)} sx={{
-                    display: 'flex',
-                    border: 'solid 1px',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    height: '5em',
-                    width: '8em',
-                    marginBottom: '1em'
+                    height: '5.3em',
+                    backgroundColor: selectedTab === i ? '#D3E1FF' : 'white',
+                    display:"flex",
+                    flexDirection:"column",
+                    alignItems:"center",
+                    justifyContent:"center"
                 }}>
-                    MCQ
+                    <Box sx={{padding:'0.5em 0.7em', border: 'solid 1px', display:'flex', flexDirection:'column', alignItems:'center', width:'fit-content'}}>
+                        <MultipleChoiceIcon/>
+                        <Typography variant="subtitle2">Multiple-Choice</Typography>
+                    </Box>
+
                 </Container>}/>
             )}
         </Tabs>
@@ -70,8 +74,8 @@ export function CreatePresentationPage() {
         <Header/>
         <Container sx={{position: 'fixed', backgroundColor: "#F0EDE0", minHeight: '100vh'}} maxWidth={false}>
             <Paper sx={{marginTop: '1em', padding: '1em 0'}}>
-                <InputBase sx={{fontSize: '40px', ":hover": {border: 'solid 1px'}, marginLeft:'0.5em'}} value={title}
-                       onChange={e => setTitle(e.target.value)}/>
+                <InputBase sx={{fontSize: '40px', ":hover": {border: 'solid 1px'}, marginLeft: '0.5em'}} value={title}
+                           onChange={e => setTitle(e.target.value)}/>
                 <Divider/>
                 <Box marginLeft={1} marginY={2}>
                     <Button variant='contained' size="small" onClick={addNewSlide}>
