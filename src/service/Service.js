8347108e721ada52a81b962e60c4643bf110ebcd
luca <lhazuca@gmail.com>
2022-11-21 15:57:25 -0300

    agregar base del backend desde env variables

--- src/service/Service.js
+++ src/service/Service.js
@@ -1,5 +1,5 @@
 export default class Service {
-  baseUrl = process.env.BACKEND_BASE_URL || "http://localhost:8080";
+  baseUrl = process.env.REACT_APP_BACKEND_BASE_URL || "http://localhost:8080";
 
   getAccessToken() {
     const loginData = localStorage.getItem(
