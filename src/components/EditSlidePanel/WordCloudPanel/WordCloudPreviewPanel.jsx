6b05092f41b897f9df30886fd9198d19054421db
luca <lhazuca@gmail.com>
2022-12-13 01:19:08 -0300

    Agregar preview panel para nube de palabras

--- src/components/EditSlidePanel/WordCloudPanel/WordCloudPreviewPanel.jsx
+++ src/components/EditSlidePanel/WordCloudPanel/WordCloudPreviewPanel.jsx
@@ -1,5 +1,6 @@
 import { Container, Paper, Typography } from "@mui/material";
 import * as React from "react";
+import ReactWordcloud from "react-wordcloud";
 
 export function WordCloudPreviewPanel({ question }) {
   const paperSx = {
@@ -15,12 +16,60 @@ export function WordCloudPreviewPanel({ question }) {
     width: "150%",
   };
 
+  const words = [
+    {
+      text: "argentina",
+      value: 20,
+    },
+    {
+      text: "aprobados",
+      value: 11,
+    },
+    {
+      text: "mundial",
+      value: 9,
+    },
+    {
+      text: "messi",
+      value: 31,
+    },
+    {
+      text: "scaloneta",
+      value: 37,
+    },
+    {
+      text: "texto",
+      value: 4,
+    },
+    {
+      text: "copa",
+      value: 20,
+    },
+    {
+      text: "qatar",
+      value: 22,
+    },
+    {
+      text: "anda'paya",
+      value: 24,
+    },
+  ];
   return (
     <Container sx={containerSx}>
       <Paper sx={paperSx}>
         <Typography mb={4} variant="h4">
           {question}
         </Typography>
+        <ReactWordcloud
+          words={words}
+          options={{
+            fontSizes: [30, 80],
+            scale: "log",
+            rotations: 1,
+            rotationAngles: [0],
+            padding: 1,
+          }}
+        />
       </Paper>
     </Container>
   );
