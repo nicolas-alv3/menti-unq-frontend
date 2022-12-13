f54b3edbe2c603d8dd054a739523b93eb711cdd3
luca <lhazuca@gmail.com>
2022-12-13 00:52:46 -0300

    Agregar handleo de tipos de slides para el panel de edicion

--- src/components/EditSlidePanel/EditSlidePanel.jsx
+++ src/components/EditSlidePanel/EditSlidePanel.jsx
@@ -1,16 +1,37 @@
 import { Box, Container, MenuItem, Select } from "@mui/material";
 import * as React from "react";
+import { useState } from "react";
 import { TabPanel } from "../TabPanel";
 import { MCQPreviewPanel } from "./MCQPanel/MCQPreviewPanel";
 import { MCQEditionPanel } from "./MCQPanel/MCQEditionPanel";
+import { WordCloudEditionPanel } from "./WordCloudPanel/WordCloudEditionPanel";
+
+const slideTypes = {
+  mcq: "MCQ",
+  wordCloud: "WORD_CLOUD",
+};
 
 export function EditSlidePanel({ index, onChange, selectedTab, slide }) {
+  const [slideType, setSlideType] = useState(slideTypes.mcq);
+
   const boxSx = {
     display: "flex",
     flexDirection: "row",
     width: "100%",
     height: "100%",
   };
+
+  function resolveEditionPanel() {
+    switch (slideType) {
+      case slideTypes.mcq:
+        return <MCQEditionPanel onChange={onChange} slide={slide} />;
+      case slideTypes.wordCloud:
+        return <WordCloudEditionPanel slide={slide} onChange={onChange} />;
+      default:
+        return <MCQEditionPanel onChange={onChange} slide={slide} />;
+    }
+  }
+
   return (
     <TabPanel
       key={`mcq-panel-${index}`}
@@ -20,13 +41,17 @@ export function EditSlidePanel({ index, onChange, selectedTab, slide }) {
       <Box sx={boxSx}>
         {/* PREVIEW */}
         <MCQPreviewPanel question={slide.question} options={slide.options} />
-        {/* TIPO DE PREGUNTA */}
         {/*  EDITAR */}
         <Container>
-          <Select value="MCQ" size="small">
-            <MenuItem value="MCQ">Multiple-Choice</MenuItem>
+          <Select
+            value={slideType}
+            size="small"
+            onChange={(e) => setSlideType(e.target.value)}
+          >
+            <MenuItem value={slideTypes.mcq}>Multiple-Choice</MenuItem>
+            <MenuItem value={slideTypes.wordCloud}>Nube de palabras</MenuItem>
           </Select>
-          <MCQEditionPanel onChange={onChange} slide={slide} />
+          {resolveEditionPanel()}
         </Container>
       </Box>
     </TabPanel>
