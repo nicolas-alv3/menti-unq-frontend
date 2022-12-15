b2dd37f1612e2f727b0aae216ae68fdc1ac7c3dc
luca <lhazuca@gmail.com>
2022-12-15 00:49:11 -0300

    Agregar pantalla para responder a word cloud

--- src/components/answers/AnswerPresentationPage.jsx
+++ src/components/answers/AnswerPresentationPage.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
-import { Button, Typography } from "@mui/material";
+import { Button, TextField, Typography } from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import PresentationService from "../../service/PresentationService";
 import { MCQAnswerSection } from "./MCQ/MCQAnswerSection";
 import { slideTypes } from "../EditSlidePanel/EditSlidePanel";
+import AnswerService from "../../service/AnswerService";
 
 function SuccessScreen() {
   return (
@@ -22,6 +23,45 @@ function SuccessScreen() {
       <Typography style={{ position: "absolute", bottom: 0 }} variant="body2">
         ¿La diapositiva no ha cambiado?<Button>Recarga la pagina</Button>
       </Typography>
+    </>
+  );
+}
+
+function WordCloudAnswersSection({
+  question,
+  slideId,
+  setSuccess,
+  startPolling,
+}) {
+  const [text, setText] = useState("");
+
+  function handleSubmit() {
+    AnswerService.answer(text, slideId).then(() => {
+      setSuccess(true);
+      startPolling();
+    });
+  }
+
+  return (
+    <>
+      <Typography style={{ marginBottom: "1em" }} variant="h5">
+        {question || ""}
+      </Typography>
+      <div>
+        <TextField
+          placeholder="Escribí aca tu respuesta"
+          sx={{ width: "40vw", marginBottom: "2em" }}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+      </div>
+      <Button
+        variant="contained"
+        sx={{ width: "24em" }}
+        onClick={() => handleSubmit()}
+      >
+        Enviar
+      </Button>
     </>
   );
 }
@@ -71,7 +111,14 @@ export function AnswerPresentationPage() {
           />
         );
       case slideTypes.wordCloud:
-        return <Typography>HOLIS</Typography>;
+        return (
+          <WordCloudAnswersSection
+            question={currentSlide.question}
+            slideId={currentSlide.id}
+            setSuccess={setSuccess}
+            startPolling={startPolling}
+          />
+        );
       default:
         return <Typography>No se encuentra tipo de slide</Typography>;
     }
